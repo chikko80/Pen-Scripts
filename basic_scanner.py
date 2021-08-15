f673c54f0c52b5ba59aced0931073b0a811cd99e
chikko80 <seitzmanuel@t-online.de>
2021-08-15 11:41:29 +0200

    recursive coments

--- basic_scanner.py
+++ basic_scanner.py
@@ -8,6 +8,10 @@ import os
 import requests
 from Wappalyzer import Wappalyzer, WebPage
 import re
+import subprocess
+import shlex
+
+subdirectories = []
 
 parser = argparse.ArgumentParser(description='Basic Scanner for CTF')
 parser.add_argument('host', help='directory to start')
@@ -25,79 +29,102 @@ END = '\033[0m'
 
 
 def menu_decorator(color_args):
-	def function_wrapper(func):
-		def draw_outlines(*args):
-			print_colored_line(color_args['color'])
-			print_colored_string(color_args['color'], f"\t{color_args['text']}")
-			print_colored_line(color_args['color'])
-			func(*args)
-			print_colored_line(color_args['color'])
-			print()
-		return draw_outlines
-	return function_wrapper
+    def function_wrapper(func):
+        def draw_outlines(*args):
+            print_colored_line(color_args['color'])
+            print_colored_string(color_args['color'], f"\t{color_args['text']}")
+            print_colored_line(color_args['color'])
+            print(color_args['color'], end='')
+            func(*args)
+            print_colored_line(color_args['color'])
+            print('\033[0m')
+        return draw_outlines
+    return function_wrapper
 
 def main():
-	print_colored_line(GREEN)
-	print_colored_string(GREEN, f"Scanning {ip}")
+    global ip
+    print_colored_line(GREEN)
+    print_colored_string(GREEN, f"Scanning {ip}")
 
-	nmap_scan(ip)
-	gobuster_scan(ip)
-	comment_scan(ip)
-	wappalyzer_scan(ip)
+    nmap_scan(ip)
+    gobuster_scan(ip)
+    comment_scan(ip)
+    wappalyzer_scan(ip)
 
 # nmap scan
-@menu_decorator({"color": CYAN, "text": "Agressiv NMAP SCAN" })
+@menu_decorator({"color": CYAN, "text": "Aggressiv NMAP SCAN" })
 def nmap_scan(ip):
-	os.system(f"nmap -A {ip}")
+    print(subprocess.getoutput(f"nmap -A {ip}"))
 
 # gobuster scan
 @menu_decorator({"color": YELLOW, "text": "Gobuster Scan" })
 def gobuster_scan(ip):
-	os.system(f"gobuster dir -u {ip} -w /usr/share/wordlists/dirb/common.txt")
+    global subdirectories
+    print(f'Scanning {ip}...')
+    output = subprocess.run(shlex.split(f"gobuster dir -u {ip} -w /usr/share/wordlists/dirb/common.txt"), stdout=subprocess.PIPE).stdout.decode('utf-8')
+    print(output)
+    output = output.split('\n')
+    for line in output:
+        line = line.strip()
+        if line.startswith('/'):
+            line = line.split()[0].replace('/', '')
+            subdirectories.append(line)
+    return subdirectories
+
 
 # comment scan
 @menu_decorator({"color": GREEN, "text": "Comments Scan" })
 def comment_scan(ip):
-	response = requests.get(f"http://{ip}/")
-	html_comments = re.findall(r'\<\!\-\-(?:.|\n|\r)*?-->', response.text)
-	css_js_comments = re.findall(r'(\/\*[\w\'\s\r\n\*]*\*\/)|(\/\/[\w\s\']*)|(\<![\-\-\s\w\>\/]*\>)', response.text)
 
-	def filter_comments(comments):
-		if type(comments[0]) == tuple:
-			return list(set([comment for tuplee in comments for comment in tuplee if comment]))
-		else:
-			return list(set(comments))
-	
-	if html_comments:
-		for comment in filter_comments(html_comments):
-			print(comment)
+    def get_comments(ip,subdirectory):
+        url = f"http://{ip}/{subdirectory}"
+        if not subdirectory:
+            url = f"http://{ip}/"
+        response = requests.get(url)
+        html_comments = re.findall(r'\<\!\-\-(?:.|\n|\r)*?-->', response.text)
+        css_js_comments = re.findall(r'(\/\*[\w\'\s\r\n\*]*\*\/)|(\/\/[\w\s\']*)|(\<![\-\-\s\w\>\/]*\>)', response.text)
 
-	if css_js_comments:
-		for comment in filter_comments(css_js_comments):
-			print(comment)
+        def filter_comments(comments):
+            if type(comments[0]) == tuple:
+                return list(set([comment for tuplee in comments for comment in tuplee if comment]))
+            else:
+                return list(set(comments))
+        
+        print(f"\nComments for {url}")
+        if html_comments:
+            for comment in filter_comments(html_comments):
+                print(comment)
 
+        if css_js_comments:
+            for comment in filter_comments(css_js_comments):
+                print(comment)
+
+    global subdirectories
+    get_comments(ip, None)
+    for subdirectory in subdirectories:
+        get_comments(ip, subdirectory)
 
 # wappalyzer scan
 @menu_decorator({"color": PURPLE, "text": "Wappalyzer Scan" })
 def wappalyzer_scan(ip):
-	webpage = WebPage.new_from_url(f"http://{ip}/")
-	wappalyzer = Wappalyzer.latest()
-	parse_wappalyzer_result(wappalyzer.analyze_with_versions_and_categories(webpage))
+    webpage = WebPage.new_from_url(f"http://{ip}/")
+    wappalyzer = Wappalyzer.latest()
+    parse_wappalyzer_result(wappalyzer.analyze_with_versions_and_categories(webpage))
 
 def parse_wappalyzer_result(result):
-	print(f"{'Name':<20} {'Category':<20} {'Name':20}")
-	print()
-	for key,inner_dict in result.items():
-		key = str(key)
-		versions = str(inner_dict['versions'])
-		categories = str(inner_dict['categories'])
-		print(f"{key:<20} {versions:<20} {categories:20}")
+    print(f"{'Name':<20} {'Category':<20} {'Name':20}")
+    print()
+    for key,inner_dict in result.items():
+        key = str(key)
+        versions = str(inner_dict['versions'])
+        categories = str(inner_dict['categories'])
+        print(f"{key:<20} {versions:<20} {categories:20}")
 
 def print_colored_line(color):
-	print(color + "---------------------------------------------------------------------------------" + '\033[0m')
+    print(color + "---------------------------------------------------------------------------------" + '\033[0m')
 
 def print_colored_string(color, string):
-	print(color + string + '\033[0m')
+    print(color + string + '\033[0m')
 
 if __name__ == '__main__':
-	main()
+    main()
